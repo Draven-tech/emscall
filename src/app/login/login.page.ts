8df0925085479275920e5bf87b8a3015585c6b5d
Zee <zack88340@gmail.com>
2025-06-17 12:55:08 +0800

    haha

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  phone: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  login() {
+    // Dummy login
+    if (this.phone && this.password) {
+      this.router.navigate(['/dashboard']);
+    }
   }
-
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  name: string = '';
+  phone: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  register() {
+    if (this.password === this.confirmPassword) {
+      // Simulate success
+      this.router.navigate(['/dashboard']);
+    }
   }
-
 }
