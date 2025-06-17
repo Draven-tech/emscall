4d6109e0f9953c71db79e130529bc2c6e9062d14
jsp3a21 <jsp3a21@scs.local>
2025-06-17 13:03:16 +0800

    hehe

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
